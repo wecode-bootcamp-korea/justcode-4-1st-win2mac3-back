bf6ed1cbfcd38fb6130785a1199119b2606c0c25
jeongseoksong <blacksjs@naver.com>
2022-04-01 17:50:05 +0900

    create : bestsellapi, allbestaellapi

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,12 @@
+
 const express = require("express");
 const router = express.Router();
 
-const productRoute = require("./productRoute");
+const productController = require("../controllers/productController");
 
-module.exports = router;
+
+router.get('/bestsell/:id', productController.bestsell)          // 차렵이불의 베스트 // 제품카테고리>침구>차렵이불 베스트용
+router.get('/allbestsell', productController.allbestsell)        // 모든제품의 베스트 // 메인메뉴 베스트용
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -14,16 +14,17 @@ app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
 
+
 const server = http.createServer(app);
 const PORT = process.env.PORT;
 
 const start = async () => {
-  // 서버를 시작하는 함수입니다.
+
   try {
     server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
   } catch (err) {
     console.error(err);
-    await prisma.$disconnect(); // 에러가 발생했을 시에 database 연결을 종료합니다.
+    await prisma.$disconnect();
   }
 };
 
